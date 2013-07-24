b61c68bb0f9fbc9914f087de785bb12e5d150aa6
Ferran Rodenas <frodenas@gmail.com>
2013-07-24 16:28:34 +0200

    rails_apps_composer: rails-bootstrap app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
 //= require turbolinks
+//= require bootstrap
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,3 @@
 CfAttUsers::Application.routes.draw do
+  root :to => 'visitors#new'
 end
